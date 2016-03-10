68fab39615c45dd7daff3478d4890be5bf6d98ed
SR3u <wnmsr3u@gmail.com>
2016-03-10 14:29:37 +0300

    Initial Commit

---
+++ Number/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Number
+//
+//  Created by Sergey Rump (SPHERE) on 10.03.2016.
+//  Copyright © 2016 marasm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
